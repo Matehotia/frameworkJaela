52da297ff2128c9e6c66beff65ea62fa69b2ddc5
Termux Kaly <75486658+Vanaty@users.noreply.github.com>
2024-10-22 07:50:43 +0200

    ok

--- src/com/annotation/GET.java
+++ src/com/annotation/GET.java
@@ -4,6 +4,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface GET {
-    String value();
+public @interface Get {
+    String url() default "";
 }
